fbd20a284deebc1209a6cc9dceeedd60da79cb44
Johannes Hüsers <S1710307067@students.fh-hagenberg.at>
2020-03-30 23:11:47 +0200

    Implement version without word weightening

--- main.cpp
+++ main.cpp
@@ -1,16 +1,20 @@
 // https://en.cppreference.com/w/cpp/container/map
-#include <map>
 #include <set>
 #include <fstream>
 #include <sstream>
 #include <iostream>
 #include <iterator>
 #include <vector>
+#include <map>
 
-using index_map_t = std::map <std::string, std::set <int>>;
-using index_map_pair_t = index_map_t::value_type;
+//using index_map_t = std::map <std::string, std::set <int>>;
+using index_set_t = std::set<std::string>;
+//using index_map_pair_t = index_map_t::value_type;
 
-std::vector<std::string> english_stopwords = {"about", "above", "after", "again", "against", "arent", "because", "been",
+const size_t MAX_SHORT_WORD_LENGTH = 3;
+const size_t NUMBER_OF_INPUT_FILES = 2;
+
+const std::vector<std::string> english_stopwords = {"about", "above", "after", "again", "against", "arent", "because", "been",
                                               "before", "being", "below", "between", "both", "cant", "cannot", "could",
                                               "couldnt", "didnt", "does", "doesnt", "doing", "dont", "down", "during",
                                               "each", "from", "further", "hadnt", "hasnt", "have", "havent", "having",
@@ -26,7 +30,7 @@ std::vector<std::string> english_stopwords = {"about", "above", "after", "again"
                                               "wont", "would", "wouldnt", "youd", "youll", "youre", "youve", "your",
                                               "yours", "yourself", "yourselves"};
 
-std::vector<std::string> german_stopwords = {"aber", "auch", "bist", "dadurch", "daher", "darum", "dass", "dein",
+const std::vector<std::string> german_stopwords = {"aber", "auch", "bist", "dadurch", "daher", "darum", "dass", "dein",
                                              "deine", "dessen", "deshalb", "dies", "dieser", "dieses", "doch", "dort",
                                              "durch", "eine", "einem", "einen", "einer", "eines", "euer", "eure",
                                              "hatte", "hatten", "hattest", "hattet", "hier", "hinter", "ihre", "jede",
@@ -76,9 +80,17 @@ std::string remove_stopwords(std::string str) {
     return str;
 }
 
+std::string remove_short_words(std::string str) {
+    if (str.length() <= MAX_SHORT_WORD_LENGTH) {
+        return "";
+    } else {
+        return str;
+    }
+}
+
 // deletes digits and puctuations and returns string in lower case
 std::string normalize(std::string str) {
-    return remove_stopwords(to_lower(purify(std::move(str)))); // functional programming
+    return remove_short_words(remove_stopwords(to_lower(purify(std::move(str)))));
 }
 
 template <typename T> class range {
@@ -116,36 +128,63 @@ struct line_string : std::string {
     }
 };
 
-int main(int argc, char *argv[]) {
-    // https://en.cppreference.com/w/cpp/io/ios_base/iostate
-    //std::ifstream file_in {"./lorem1000.txt"};
-    std::ifstream file_in = (std::ifstream)argv[1];
-    int nr {0};
-    index_map_t index {};
-
+void add_to_index(std::ifstream &file, index_set_t &index) {
     // loop through all lines
-    for (auto const &line : make_range <std::string> (file_in)) { // everything that has a begin and an end can be on the the right side of the colon
+    for (auto const &line : make_range <std::string> (file)) { // everything that has a begin and an end can be on the the right side of the colon
         std::istringstream line_in {line};
-        ++nr;
 
         // loop through all words of a line
         for (std::string const &word : make_range <std::string> (line_in)) {
             // insert word into index
             if (auto w {normalize(word)}; !std::empty(w)) { // variable only scoped in if statement
-                index[w].insert(nr);
+                index.insert(w);
             }
         }
     }
+}
 
-    // ouput
+void print_index(index_set_t index) {
     // loop through each entry in the index
-    for (auto const & [word, numbers] : index) {
-        std::cout << word << ": ";
-        for (int const &n : numbers) {
-            std::cout << n << ' ';
-        }
-        std::cout << "\n";
-    }
+//    for (auto const &word : index) {
+//        std::cout << word << "\n";
+//    }
     std::cout << "======================\n";
     std::cout << "Total: " << index.size() << " words.\n";
+}
+
+void print_result(double first, double second, double intersection) {
+    double degree_of_conformity = 0;
+    if (first && second && intersection) {
+        std::cout << "intersection: " << intersection << ", first: " << first << "\n";
+        std::cout << "intersection / first: " << intersection / first << "\n";
+        degree_of_conformity = (double)((((intersection / first) + (intersection / second))) / NUMBER_OF_INPUT_FILES) * 100;
+    }
+    std::cout << "Number of significant words matching: " << intersection << "\n";
+    std::cout << "Degree of conformity:\n";
+    std::cout << "[";
+    for (size_t i = 0; i < (size_t)degree_of_conformity; ++i) {
+        std::cout << "#";
+    }
+    for (size_t i = (size_t)degree_of_conformity; i < 100; ++i) {
+        std::cout << "-";
+    }
+    std::cout << "] " << degree_of_conformity << "%\n";
+}
+
+void run_without_word_weightening(std::ifstream &first, std::ifstream &second) {
+    index_set_t index1 {};
+    index_set_t index2 {};
+    index_set_t result_index {};
+
+    add_to_index(first, index1);
+    add_to_index(second, index2);
+
+    std::set_intersection(index1.begin(), index1.end(), index2.begin(), index2.end(), std::inserter(result_index, result_index.end()));
+    print_result(index1.size(), index2.size(), result_index.size());
+}
+
+int main(int argc, char *argv[]) {
+    std::ifstream first_file = (std::ifstream)argv[1];
+    std::ifstream second_file = (std::ifstream) argv[2];
+    run_without_word_weightening(first_file, second_file);
 }
